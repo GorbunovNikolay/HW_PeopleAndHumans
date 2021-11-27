9601f491d3a461a71b30e570caa3d4a26656e4cb
Gorbunov Nikolay <gor_bunk@mail.ru>
2021-11-27 16:27:55 +0300

    Добавлен исполняемый код

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,27 @@
 
 fun main() {
-    
+    var likes = 0
+    var maxLikes = 100
+
+    while (likes < maxLikes) {
+        var result = addAnEnding(likes)
+        println("Понравилось $likes $result")
+        likes += 1
+    }
+}
+
+fun addAnEnding(likes: Int): String {
+    var n = likes
+    n %= 100
+    if (n >= 5 && n <= 20) {
+        return "людям"
+    }
+    n %= 10
+    if (n === 1) {
+        return "человеку"
+    }
+    if (n >= 2 && n <= 4) {
+        return "людям"
+    }
+    return "людям"
 }
